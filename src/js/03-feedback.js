eadb604166c83115ce34f72e4805565accf7281f
VitaliySlivchuk <41226137+VitaliySlivchuk@users.noreply.github.com>
2022-12-18 14:21:33 +0200

    main

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const FEEDBACK = 'feedback-form-state';
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onSetDataonLocalStorage, 500));
+
+let formData = {};
+
+function onSetDataonLocalStorage(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK, JSON.stringify(formData));
+}
+
+if (localStorage.getItem(FEEDBACK)) {
+  formData = JSON.parse(localStorage.getItem(FEEDBACK));
+  for (let key in formData) {
+    formEl.elements[key].value = formData[key];
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.clear();
+  console.log(formData);
+}
